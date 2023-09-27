56c4a0b40cd75e66d47c3fda9b586844a0fd8da3
domingo2000 <domingo.edwards@uc.cl>
2023-09-26 23:39:14 -0300

    feature: api dockerized

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// GET endpoint
+app.get("/", (req, res) => {
+  res.send("GET request received at /");
+});
+
+// GET endpoint
+app.get("/healthcheck", (req, res) => {
+  res.send(JSON.stringify({ message: true }));
+});
+
+// POST endpoint
+app.post("/", (req, res) => {
+  res.send(
+    `POST request received at / with message: ${JSON.stringify(req.body)}`
+  );
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
