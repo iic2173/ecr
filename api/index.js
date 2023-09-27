195dfdf30417e728eec1baa1fcd41e0665788a0c
domingo2000 <domingo.edwards@uc.cl>
2023-09-27 01:30:04 -0300

    feature: GET / v3

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 // GET endpoint
 app.get("/", (req, res) => {
-  res.send("GET request received at / v2");
+  res.send("GET request received at / v3");
 });
 
 // GET endpoint
